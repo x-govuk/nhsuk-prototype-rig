50b4ec1d16b0c4400417d1c850fbe46fd2674ee7
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-11-28 22:06:38 +0000

    Reinstate nodemon to restart development server

--- bin/cli.js
+++ bin/cli.js
@@ -1,58 +1,21 @@
 #!/usr/bin/env node
-import process from 'node:process'
+import nodemon from 'nodemon'
 
-import * as esbuild from 'esbuild'
-import { sassPlugin } from 'esbuild-sass-plugin'
+nodemon({
+  script: './node_modules/nhsuk-prototype-rig/lib/server.js',
+  ignore: ['assets/*', 'app/assets/*', 'node_modules/*']
+})
 
-import { browserSyncConfig } from '../lib/browser-sync.js'
-import { findAvailablePort } from '../lib/environment.js'
-import app from '../lib/server.js'
+let ignoreExit = false
 
-// Arguments
-const args = process.argv.slice(2)
-const watchMode = args.includes('--watch')
+nodemon.on('restart', () => {
+  ignoreExit = true
+})
 
-async function compileAssets() {
-  const options = {
-    entryPoints: [
-      'app/assets/javascripts/application.js',
-      'app/assets/stylesheets/application.scss'
-    ],
-    entryNames: '[name]',
-    bundle: true,
-    legalComments: 'none',
-    minify: true,
-    outdir: 'assets',
-    plugins: [
-      sassPlugin({
-        loadPaths: ['.', 'node_modules'],
-        silenceDeprecations: ['import', 'mixed-decls'],
-        quietDeps: true
-      })
-    ],
-    sourcemap: true
+nodemon.on('exit', () => {
+  if (ignoreExit) {
+    ignoreExit = false
+    return
   }
-
-  try {
-    if (watchMode) {
-      const ctx = await esbuild.context(options)
-      await ctx.watch()
-    } else {
-      await esbuild.build(options)
-    }
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-findAvailablePort(async (port) => {
-  if (watchMode) {
-    const proxyPort = port - 50
-    app.listen(proxyPort, () => browserSyncConfig(app, port, proxyPort))
-    await compileAssets()
-  } else {
-    await compileAssets()
-    console.info(`Listening on port ${port}`)
-    app.listen(port)
-  }
+  process.exit()
 })
